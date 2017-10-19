4b09cdaf29e0508d75ab237f73a36c80cbf17c55
roscian Frank <roscian.frank@babylonhealth.com>
2017-10-19 13:17:50 +0100

    few more examples

---
+++ src/test/java/genericsChallenge/FootballPlayer.java
@@ -0,0 +1,11 @@
+package genericsChallenge;
+
+/**
+ * Created by dev on 17/10/2015.
+ */
+public class FootballPlayer extends Player {
+
+    public FootballPlayer(String name) {
+        super(name);
+    }
+}
